c40814b49c08f2aa1040fe0c65b94b60d880ae55
tuanminh1999 <tuanminh01659889@gmail.com>
2022-03-01 10:02:14 +0700

    build api get loai nguoi dung
    use mapstruct, remove HttpMethodsUtil

--- src/main/java/cybersoft/javabackend/moviejava14/loaiNguoiDung/service/LoaiNguoiDungService.java
+++ src/main/java/cybersoft/javabackend/moviejava14/loaiNguoiDung/service/LoaiNguoiDungService.java
@@ -1,5 +1,6 @@
 package cybersoft.javabackend.moviejava14.loaiNguoiDung.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import cybersoft.javabackend.moviejava14.loaiNguoiDung.dto.LoaiNguoiDungDTO;
@@ -7,5 +8,7 @@ import cybersoft.javabackend.moviejava14.loaiNguoiDung.dto.LoaiNguoiDungDTO;
 public interface LoaiNguoiDungService {
 
 	Optional<LoaiNguoiDungDTO> findByTenLoaiNguoiDung(String tenLoaiNguoiDung);
+
+	List<LoaiNguoiDungDTO> findAll();
 
 }

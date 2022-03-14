72149c79972fb5eece7e549f6a09abd384d6b979
tuanminh1999 <tuanminh01659889@gmail.com>
2022-03-14 18:57:45 +0700

    delete CumRapControllerImpl

---
+++ src/main/java/cybersoft/javabackend/moviejava14/cumRap/controller/CumRapController.java
@@ -0,0 +1,8 @@
+package cybersoft.javabackend.moviejava14.cumRap.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CumRapController {
+
+}
